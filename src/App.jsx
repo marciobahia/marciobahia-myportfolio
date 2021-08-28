345e9d59bb664a840c03f45e9aeb437c4b2dcea1
Marcio Bahia <marciosellabahia@hotmail.com>
2021-08-28 15:55:11 -0300

    Add App.jsx and index.html

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import "./app.scss"
+
 function App() {
   return (
     <div className="app">
