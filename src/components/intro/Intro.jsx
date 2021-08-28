fdf8212490f14829cf7c873f11529bc7b33169f3
Marcio Bahia <marciosellabahia@hotmail.com>
2021-08-27 22:45:38 -0300

    initial

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,10 @@
+import "./intro.scss"
+
+export default function Intro() {
+    return (
+        <div className="intro">
+            Márcio Sella Bahia
+        </div>
+        
+    )
+}
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Portfolio() {
+  return (
+  
+  <div className="portfolio">
+
+  </div>
+  )
+}
+
